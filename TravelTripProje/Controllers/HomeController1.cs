e0697311db7945ef104f6a7ea6d962d76641cc94
Yusufaltunn <yusuf99altun@hotmail.com>
2023-09-09 10:06:33 +0300

    Proje dosyası ekle.

---
+++ TravelTripProje/Controllers/HomeController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelTripProje.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
