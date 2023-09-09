c389253719c7bf61e056cefaa22d9312fff437e0
Yusufaltunn <yusuf99altun@hotmail.com>
2023-09-09 10:24:12 +0300

    code first 2

---
+++ TravelTripProje/Models/Siniflar/Yorumlar.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelTripProje.Models.Siniflar
+{
+	public class Yorumlar
+	{
+		[Key]
+		public int ID { get; set; }
+		public string KullaniciAdi { get; set; }
+		public string Mail { get; set; }
+		public string Yorum { get; set; }
+		public int BLOGID { get; set; }
+	}
+}
